84db6dc7277cee87b2def1aafbe4e686641a7092
artucorreia <115330684+artucorreia@users.noreply.github.com>
2024-09-02 21:47:38 -0300

    csharp

---
+++ FirstProject/FirstProject/Product.cs
@@ -0,0 +1,49 @@
+﻿namespace FirstProject {
+    internal class Product {
+        public string Name { get; private set; }
+        private double _price;
+        private int _stock;
+        
+        public Product(string name, double price, int stock) { 
+            Name = name;
+            _price = price;
+            _stock = stock;
+        }
+
+        public double Price {
+            get { return _price; }
+            set {
+                if (
+                    _price != value 
+                    && value >= 0
+                ) {
+                    _price = value;
+                }
+            }
+        }
+
+        public int Stock { 
+            get { return _stock; }
+            private set {
+                if (
+                    _stock != value 
+                    && value >= 0
+                ) {
+                    _stock = value;
+                }
+            }
+        }
+
+        public void RemoveProdutc(int quantity) {
+            Stock -= quantity;
+        }
+
+        public void AddProduct(int quantity) {
+            Stock += quantity;
+        }
+
+        public override string ToString() {
+            return $"{Name}, price: {_price}, stock: {_stock}";
+        }
+    }
+}
---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace FirstProject {
+    internal class Program {
+        static void Main(string[] args) {
+            Nullable<double> a = null;
+            Console.WriteLine(a);
+            double? b = null;
+            Console.WriteLine(b);
+        }
+    }
+}
